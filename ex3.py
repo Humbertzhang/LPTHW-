22f53d3e4dd905915251e99b527a7179e57dbba7
Humbertzhang <504490160@qq.com>
2016-12-07 18:28:49 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,39 @@
+#--coding:utf-8--
+#在屏幕中显示文字
+print "I will now count my chickens:"
+#显示文字+算数
+print "Hens",25+30/6
+#算75取余4，再用100-那个数
+print "Roosters",100-25*3%4
+
+print "Now I will count my eggs"
+#计算
+print 3+2+1-5+4%2-1/4+6
+print "Is it true that 3+2 < 5-7?"
+# ＜　＞　＜＝　＞＝　在“”外面时会自动判断对错，在屏幕上显示　True or False
+print 3+2<5-7
+print "What is 3+2?",3+2
+print "What is 5-7?",5-7
+print "Oh,taht's why it's False."
+print "How about some more."
+print "Is it greater ?",5>-2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal?",5<=-2
+
+
+#浮点数版
+
+
+print "Hens",25.00+30.00/6.00
+print "Roosters",100.00-25.00*3.00%4.00
+print "Now I will count my eggs"
+print 3.00+2.00+1.00-5.00+4.00%2.00-1.00/4.00+6.00
+print "Is it true that 3+2 < 5-7?"
+print 3+2<5-7
+print "What is 3+2?",3+2
+print "What is 5-7?",5-7
+print "Oh,taht's why it's False."
+print "How about some more."
+print "Is it greater ?",5>-2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal?",5<=-2
