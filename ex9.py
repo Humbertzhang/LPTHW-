e367c146b4eeed00fc85ca752795c932d3664b32
Humbertzhang <504490160@qq.com>
2016-12-08 00:08:28 +0800

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,18 @@
+#--coding:utf-8--
+#Here's some new strang stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJul\nAug"
+
+print "Here are the days: ",days
+print "Here are the months: ",months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want,or 5,or 6.
+"""
+
+#1， \n 仍表示换行
+#2， """ 与 """ 之间可以放任意多的字符串  
