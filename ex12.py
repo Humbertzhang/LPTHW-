ddab2c9659a9349161859304da3479cc946832ee
Humbertzhang <504490160@qq.com>
2016-12-08 18:43:25 +0800

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,47 @@
+#-coding:utf-8--
+
+age = raw_input("How old are you? ")
+
+height = raw_input("How tall are you? ")
+
+weight = raw_input("How much do you weight? ")
+
+print "So, you are %r old, %r tall and %r heavy." %(age,height,weight)
+
+#第一题
+#输入 python -m pydoc raw_input 后输出：
+#PS C:\LPTHW> python -m pydoc raw_input
+#Help on built-in function raw_input in module __builtin__:
+
+#raw_input(...)
+    #raw_input([prompt]) -> string
+
+    #Read a string from standard input.  The trailing newline
+    #If the user hits EOF (Unix: Ctl-D, Windows: Ctl-Z+Return
+    #On Unix, GNU readline is used if enabled.  The prompt st
+    #is printed without a trailing newline before reading.
+
+
+#第三题	
+#Windows 下PYDOC 的使用：
+#在Shell进入的文件夹中输入：  python -m pydoc <modulename>
+#pydoc模块主要用来从Python模块中提取信息并生成文档。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
