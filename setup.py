e17cb588cfae54e35ec6108eb75b677840a7db61
Humbertzhang <504490160@qq.com>
2016-12-15 08:56:15 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+	
+config = {
+'description' : 'My Project',
+'author' : 'Zhangke',
+'url':'URL to get it at.',
+'download_url':'Where to download it.',
+'author_email':'504490160@qq.com',
+'version':'0.1',
+'install_requires':['nose'],
+'packages':['NAME'],
+'scripts':[]
+'name':'projectname'
+}
+
+setup(**config)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
