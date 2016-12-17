a39be27de4a4425d36908ab4cdac63c773652f1d
Humbertzhang <504490160@qq.com>
2016-12-17 21:37:11 +0800

    Add files via upload

--- 52/map.py
+++ 52/map.py
@@ -29,6 +29,13 @@ Armory and about to pull a weapon to blast you.
 
 laser_weapon_armory=Room("Laser Weapon Armory",
 """
+Lucky for you they made you learn Gothon insults in the academy.
+You tell the one Gothon joke you know:
+Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr.
+The Gothon stops, tries not to laugh, then busts out laughing and can't move.
+While he's laughing you run up and shoot him square in the head"
+putting him down, then jump through the Weapon Armory door.
+
 You do a dive roll into the Weapon Armory, crouch and scan the room
 for more Gothons that might be hiding.  It's dead quiet, too quiet.
 You stand up and run to the far side of the room and find the
@@ -40,6 +47,10 @@ get the bomb.  The code is 3 digits.
 
 the_bridge = Room("The Bridge",
 """
+The container clicks open and the seal breaks, letting gas out.
+You grab the neutron bomb and run as fast as you can to the
+bridge where you must place it in the right spot.
+
 You burst onto the Bridge with the netron destruct bomb
 under your arm and surprise 5 Gothons who are trying to
 take control of the ship.  Each of them has an even uglier
@@ -50,6 +61,15 @@ arm and don't want to set it off.
 
 escape_pod=Room("Escape Pod",
 """
+You point your blaster at the bomb under your arm"
+and the Gothons put their hands up and start to sweat."
+You inch backward to the door, open it, and then carefully"
+place the bomb on the floor, pointing your blaster at it."
+You then jump back through the door, punch the close button"
+and blast the lock so the Gothons can't get out."
+Now that the bomb is placed you run to the escape pod to"
+get off this tin can."
+
 You rush through the ship desperately trying to make it to
 the escape pod before the whole ship explodes.  It seems like
 hardly any Gothons are on the ship, so your run is clear of
@@ -125,10 +145,13 @@ your head and eats you.
 ###################################DeathEnd################
 
 
+
 the_bridge.add_paths({
 	'throw the bomb':deathone,  ############# death1
 	'slowly place the bomb':escape_pod
+	
 })
+
 
 laser_weapon_armory.add_paths({
 	'123':the_bridge,
@@ -142,15 +165,6 @@ central_corridor.add_paths({
 })
 
 START = central_corridor
-
-
-
-
-
-
-
-
-
 
 
 
